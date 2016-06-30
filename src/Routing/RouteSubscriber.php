d04276c56adc468f7b9801c79886986388eb1ba7
Patrick Weston <weston@palantir.net>
2016-06-30 10:39:53 -0500

    Add support for menu_admin_per_menu

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -19,8 +19,24 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   public function alterRoutes(RouteCollection $collection) {
     // Reroute the menu edit form to one using bigmenu.
-    if ($route = $collection->get('entity.menu.edit_form')) {
-      $route->setPath('/admin/structure/menu/manage/{menu}/bigmenu');
+    $routes = $collection->all();
+    foreach ($routes as $route_name => $route) {
+      switch ($route_name) {
+        case 'entity.menu.edit_form':
+        case 'entity.menu.add_link_form':
+          $route->setPath('/admin/structure/menu/manage/{menu}/bigmenu');
+          if (\Drupal::moduleHandler()->moduleExists('menu_admin_per_menu')) {
+            $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menuAccess']);
+          }
+          break;
+
+        case 'bigmenu.menu':
+        case 'bigmenu.menu_link':
+          if (\Drupal::moduleHandler()->moduleExists('menu_admin_per_menu')) {
+            $route->setRequirements(['_custom_access' => '\Drupal\menu_admin_per_menu\Access\MenuAdminPerMenuAccess::menuAccess']);
+          }
+          break;
+      }
     }
   }
 
